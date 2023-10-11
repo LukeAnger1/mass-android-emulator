83d9434cf15fa840764ce74a1668148d79843c4c
help <anger.luke2004@gmail.com>
2023-10-10 23:15:37 -0400

    These are the python files I am commiting. I want the main goal to be to make a bot net of androids that can run scripts. IE free laundry but easily expandable to other scripts through the record feature where user input is recorded through adb command

---
+++ config.py
@@ -0,0 +1,5 @@
+# This is where are the variables are stored in case your setup is different for the terminal commands
+
+sdkmanager = "./cmdline-tools/latest/bin/sdkmanager"
+avdmanager = "./cmdline-tools/latest/bin/avdmanager"
+adb = "./platform-tools/adb"
---
+++ manage_avds.py
@@ -0,0 +1,96 @@
+import subprocess
+import config
+import re
+
+def list_known_devices():
+    try:
+        # Execute the avdmanager command to list known devices
+        result = subprocess.run([config.avdmanager, 'list', 'avd'], capture_output=True, text=True)
+        
+        # Check if the command was successful
+        if result.returncode == 0:
+            # print("Known devices:")
+            # print(result.stdout)
+            return result.stdout
+        else:
+            print("Error listing known devices:")
+            print(result.stderr)
+    
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def create_virtual_device(name = None, device = "pixel_3a", package = "system-images;android-34;google_apis;x86_64"):
+
+    # this will create the next device in the list 
+    if name == None:
+        # this takes the output from list_known_devices and finds its length to find the number for the next device. Then converts it to a str
+        name = str(len(re.findall(r'Name:\s+(.+)', list_known_devices())))
+
+    try:
+        # Execute the avdmanager command to create a new virtual device
+        command = [
+            config.avdmanager,
+            'create',
+            'avd',
+            '--name', name,
+            '--device', device,
+            '--package', package
+        ]
+        result = subprocess.run(command, capture_output=True, text=True)
+        
+        # Check if the command was successful
+        if result.returncode == 0:
+            print(f"Virtual device '{name}' created successfully.")
+        else:
+            print(f"Error creating virtual device '{name}':")
+            print(result.stderr)
+            print('if device name is number, retrying with a higher number')
+            if name.isdigit():
+                print('running again with one higher name')
+                create_virtual_device(name = str(int(name) + 1))
+    
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def remove_virtual_device(avd_name):
+    try:
+        # Execute the avdmanager command to delete the specified AVD
+        command = [
+            config.avdmanager,
+            'delete',
+            'avd',
+            '--name', avd_name
+        ]
+        result = subprocess.run(command, capture_output=True, text=True)
+        
+        # Check if the command was successful
+        if result.returncode == 0:
+            print(f"AVD '{avd_name}' removed successfully.")
+        else:
+            print(f"Error removing AVD '{avd_name}':")
+            print(result.stderr)
+    
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def install_apk(avd_name, apk_path):
+    try:
+        # Use adb to install the APK on the specified AVD by name
+        adb_install_command = [config.adb, '-s', f'emulator-{avd_name}', 'install', '-r', apk_path]
+        result = subprocess.run(adb_install_command, capture_output=True, text=True)
+        
+        # Check if the installation was successful
+        if "Success" in result.stdout:
+            print(f"APK '{apk_path}' installed successfully on '{avd_name}'.")
+        else:
+            print(f"Error installing APK '{apk_path}' on '{avd_name}':")
+            print(result.stderr)
+    
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+if __name__ == '__main__':
+    # Example usage
+    list_known_devices()
+    create_virtual_device()
+    print(list_known_devices())
+    install_apk('3', 'CSC GO Laundry_1.17.0_Apkpure.apk')

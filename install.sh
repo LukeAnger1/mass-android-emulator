ac4a78392851782c4f6498b7b17d17789905415e
Luke Anger <anger.luke2004@gmail.com>
2024-01-28 16:18:46 -0800

    This will install stuff and start the installation, but there is a problem where the cmdline tools need to be in a folder called latest

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This installation script is intended to be run on a Linux machine.
-# NOTE: may need to run with sudo privelege
+# NOTE: needs to run with sudo privelege
 
 echo "starting the installation"
 
@@ -11,6 +11,27 @@ apt install unzip -y
 apt install openjdk-17-jdk wget unzip -y
 
 echo "getting the main package"
-wget https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip
-unzip commandlinetools-linux-10406996_latest.zip
+# IMPORTANT TODO: uncomment the below
+# wget https://dl.google.com/android/repository/commandlinetools-linux-10406996_latest.zip
+# unzip commandlinetools-linux-10406996_latest.zip
 
+# Get the current directory of the script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Append "cmdline-tools" folder to the ANDROID_SDK_ROOT
+ANDROID_SDK_ROOT="$SCRIPT_DIR/cmdline-tools"
+
+# Set ANDROID_SDK_ROOT environment variable
+echo "export ANDROID_SDK_ROOT=\"$ANDROID_SDK_ROOT\"" >> ~/.bashrc
+source ~/.bashrc
+
+echo "Main installation completed"
+
+# Navigate to the cmdline-tools/bin directory
+cd "$ANDROID_SDK_ROOT/bin"
+
+# Install the required SDK packages using sdkmanager
+./sdkmanager --install "platform-tools"
+./sdkmanager --install "emulator"
+
+echo "Installing the needed tools"
